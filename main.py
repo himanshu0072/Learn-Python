972470db161893d5754bd0d85a77af1d8691d9c7
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-03-19 00:16:08 +0530

    Concept of Access Modifier in Python

--- main.py
+++ main.py
@@ -2077,23 +2077,76 @@
 #     print(5 + 5)
 
 # inheritance in Python
+#
+# class Employee:
+#     def __init__(self, name, id):
+#         self.name = name
+#         self.id = id
+#
+#     def showDetails(self):
+#         print(f"The Employee with ID = {self.id} is {self.name}.")
+#
+#
+# class Programmer(Employee):  # inheritance
+#     def showLang(self):
+#         print(f"He knows Python")
+#
+#
+# e = Employee("Himanshu Prajapati", 2)
+# e.showDetails()
+# e = Programmer("Priya", 20)
+# e.showDetails()
+# e.showLang()
 
-class Employee:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
-
-    def showDetails(self):
-        print(f"The Employee with ID = {self.id} is {self.name}.")
 
 
-class Programmer(Employee):  # inheritance
-    def showLang(self):
-        print(f"He knows Python")
+# Access Modifier in Python
+
+#protected
+
+# class Student:
+#     def __init__(self):
+#         self._name = "Harry"
+#
+#     def _funName(self):      # protected method
+#         return "CodeWithHarry"
+#
+# class Subject(Student):       #inherited class
+#     pass
+#
+# obj = Student()
+# obj1 = Subject()
+#
+# # calling by object of Student class
+# print(obj._name)
+# print(obj._funName())
+# # calling by object of Subject class
+# print(obj1._name)
+# print(obj1._funName())
 
 
-e = Employee("Himanshu Prajapati", 2)
-e.showDetails()
-e = Programmer("Priya", 20)
-e.showDetails()
-e.showLang()
+#private
+# class Student:
+#     def __init__(self, age, name):
+#         self.__age = age  # An indication of private variable
+#
+#         def __funName(self):  # An indication of private function
+#             self.y = 34
+#             print(self.y)
+#
+#
+# class Subject(Student):
+#     pass
+#
+#
+# obj = Student(21, "Harry")
+# obj1 = Subject
+#
+# # calling by object of class Student
+# print(obj.__age)
+# print(obj.__funName())
+#
+# # calling by object  of class Subject
+# print(obj1.__age)
+# print(obj1.__funName())
+
