d638eaca24f2ffac3f5a3f2936e25b78c0de5847
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-03-20 01:03:45 +0530

    Flatten and transpose of array

--- main.py
+++ main.py
@@ -2257,33 +2257,52 @@
 
 # Array Dimensions
 
-# 1. Using shape to get array dimensions
+# # 1. Using shape to get array dimensions
+# import numpy
+#
+# my_1D_array = numpy.array([1, 2, 3, 4, 5])
+# print(my_1D_array.shape)     #(5,) -> 1 row and 5 columns
+#
+# my_2D_array = numpy.array([[1, 2],[3, 4],[6,5]])
+# print(my_2D_array.shape)    #(3, 2) -> 3 rows and 2 columns
+#
+#
+#
+# # 2. Using shape to change array dimensions
+#
+#
+# import numpy
+#
+# change_array = numpy.array([1,2,3,4,5,6])
+# change_array.shape = (3, 2)
+# print(change_array)
+
+#
+# import numpy
+#
+#
+# lists = list(map(int, input().split()))
+# print(numpy.reshape(lists,(3,3)))
+
+
+# Transpose
+
 import numpy
+my_array = numpy.array([[1,2,3],[4,5,6]])
+print(numpy.transpose(my_array))
 
-my_1D_array = numpy.array([1, 2, 3, 4, 5])
-print(my_1D_array.shape)     #(5,) -> 1 row and 5 columns
-
-my_2D_array = numpy.array([[1, 2],[3, 4],[6,5]])
-print(my_2D_array.shape)    #(3, 2) -> 3 rows and 2 columns
-
-
-
-# 2. Using shape to change array dimensions
-
-
+# Flantten
 import numpy
-
-change_array = numpy.array([1,2,3,4,5,6])
-change_array.shape = (3, 2)
-print(change_array)
+my_array = numpy.array([[1,2,3],[4,5,6]])
+print(my_array.flatten())
 
 
+import numpy as np
 
-
-
-
-
-
+n, m = map(int, input().split())
+data = np.array([input().split() for _ in range(n)], int)
+print(np.transpose(data))
+print(data.flatten())
 
 
 
